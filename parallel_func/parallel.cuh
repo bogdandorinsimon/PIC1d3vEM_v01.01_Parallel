3743e7171a07193d2691a09b961e8b378efc319d
DESKTOP-PV3BAKL\bogdan <simonbogdandorin@gmail.com>
2021-04-19 02:24:45 +0300

    fix parallel.cuh definition

--- parallel_func/parallel.cuh
+++ parallel_func/parallel.cuh
@@ -1,7 +1,3 @@
-#include "bfield.cu"
 #include "cuda_runtime.h"
 
-#if !defined( _RVGS_ )
-#define _RVGS_
 cudaError_t bfieldWithCuda(double* h_by, double* h_bz, double* h_ey, double* h_ez, int m, double c);
-#endif
