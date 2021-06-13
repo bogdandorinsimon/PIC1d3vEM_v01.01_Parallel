fd812b67549e1cedc9aa9e1ee019ebc5472430e3
DESKTOP-PV3BAKL\bogdan <simonbogdandorin@gmail.com>
2021-06-14 01:59:28 +0300

    current step parallelization

--- parallel_func/parallel.cuh
+++ parallel_func/parallel.cuh
@@ -1,5 +1,20 @@
 #include "cuda_runtime.h"
 
+#if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
+#else
+__device__ double atomicAdd(double* address, double val)
+{
+    unsigned long long int* address_as_ull = (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+    do {
+        assumed = old;
+        old = atomicCAS(address_as_ull, assumed,
+            __double_as_longlong(val + __longlong_as_double(assumed)));
+    } while (assumed != old);
+    return __longlong_as_double(old);
+}
+#endif
+
 cudaError_t bfieldWithCuda(double* h_by, double* h_bz, double* h_ey, double* h_ez, int m, double c);
 cudaError_t efieldWithCuda(
 	double* h_ex, double* h_ey, double* h_ez,
